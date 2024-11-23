6c5d89c6cbcfd6c2316f5935774c96c1930fd255
Orkun Ozturk <orkunozturk@Orkuns-MacBook-Pro.local>
2024-11-23 18:17:16 +0800

    content fix

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -9,34 +9,33 @@ const services = [
   {
     title: "SEO内容",
     description:
-      "SEO的目的在于精准定位相关话题，并与受众分享足够多次的独到见解。Up to 15x organic site visits in less than 9 months!",
+      "SEO的目的在于精准定位相关话题，并与受众分享足够多次的独到见解。9个月内实现高达15倍的自然访问量提升！",
     icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
   },
   {
     title: "邮件营销",
-    description:
-      "We create high conversion email campaigns that will convince people to buy from you",
+    description: "我们创建高转化率的邮件营销活动，帮助说服用户购买您的产品。",
     icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
   },
   {
     title: "媒体营销",
-    description: "Highly engaging social media campaigns, KOL endorsements. ",
+    description: "高度吸引力的社交媒体活动，KOL背书。",
     icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
   },
   {
-    title: "Landing Pages and B2B Offers",
+    title: "落地页和B2B内容",
     description:
-      "We create long-form content like ebooks, whitepapers etc. to demonstrate your thought leadership to B2B leads.",
+      "我们制作长篇内容，例如电子书和白皮书，向B2B潜在客户展示您的专业领导力。",
     icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
   },
   {
-    title: "English and Communication Training",
+    title: "英语和沟通培训",
     description:
-      "Let your staff do more than just onboarding. We will equip your team to be more efficient overseas sales people.",
+      "让您的员工不仅仅是完成入职流程。我们将帮助您的团队成为更高效的海外销售人员。",
     icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
   },
